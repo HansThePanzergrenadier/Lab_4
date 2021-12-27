3e772e0c8d5a65f32a4c5ae2656f64d05ad49e70
HansThePanzergrenadier <49830583+HansThePanzergrenadier@users.noreply.github.com>
2021-12-27 23:56:30 +0200

    Updated unity scripts

---
+++ Server/Scripts/NickController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NickController : MonoBehaviour
+{
+    //GooController parCon;
+    void Start()
+    {
+        //parCon = GetComponentInParent<GooController>();
+    }
+
+    void Update()
+    {
+
+    }
+}
